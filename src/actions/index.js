8ec29417f505704c5aea3afe07ac85fe17fa7cbe
Subham Yadav <subhamy142@gmail.com>
2020-12-19 22:36:55 +0530

    added action types and creators

---
+++ src/actions/index.js
@@ -0,0 +1,11 @@
+//action type 
+export const ADD_MOVIES = 'ADD_MOVIES'
+
+
+//action creators
+ export function addMovies(movies){
+     return{
+        type:ADD_MOVIES,
+        movies:movies
+      }
+ }
